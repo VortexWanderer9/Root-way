6ca8f8aa546237ba7ac60733932ee39eeb7845dd
Tyrell Wellick <keshavniure99@gmail.com>
2025-09-03 08:47:41 -0800

    add error message section

---
+++ script/login.js
@@ -0,0 +1,6 @@
+const button = document.querySelector(".button");
+const email = document.querySelector('#email-input');
+const password = document.querySelector('#password-input');
+button.addEventListener('click', () =>{
+    
+})
