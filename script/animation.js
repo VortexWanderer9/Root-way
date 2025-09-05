767b62712e49b5d6ca8de4b84113e276483a7eee
Tyrell Wellick <keshavniure99@gmail.com>
2025-09-04 23:21:32 -0800

    typeWrite animation

--- script/animation.js
+++ script/animation.js
@@ -7,3 +7,19 @@ function rotateGradient(){
 }
 rotateGradient()
 
+let title = document.querySelector('.heading');
+let i = 0;
+const text = 'Login First'
+ function typeWrite(){
+    if(i < text.length){
+        title.textContent = text.substring(0, i +1);
+        i++;
+        setTimeout(typeWrite, 200)
+    }
+    if(i == text.length){
+            i--
+    }
+ }
+
+
+ typeWrite()
