1c51379feddac18f226e180ec80618e672b62e37
Tyrell Wellick <keshavniure99@gmail.com>
2025-09-04 22:40:57 -0800

    fix bug

--- script/login.js
+++ script/login.js
@@ -3,5 +3,9 @@ const email = document.querySelector('#email-input');
 const password = document.querySelector('#password-input');
 
 button.addEventListener('click', (e) =>{
+    e.preventDefault();
+    let user = email.value.trim()
+    console.log(user);
+    
           
 })
