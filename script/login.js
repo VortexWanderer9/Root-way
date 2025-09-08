572716f2fd238a1836a98b7bca9f56b34542ea13
Tyrell Wellick <keshavniure99@gmail.com>
2025-09-08 07:56:17 -0800

    add form validation[

--- script/login.js
+++ script/login.js
@@ -1,15 +1,22 @@
 const button = document.querySelector(".button");
 const email = document.querySelector('#email-input');
 const password = document.querySelector('#password-input');
+const error = document.querySelector('.error-message');
 
 
 
 
 button.addEventListener('click', (e) =>{
     e.preventDefault();
-    let user = email.value.trim()
-    console.log(user);
-    
-          
-});
+    let user = email.value.trim();
+    let pass = password.value.trim();
+    if(user === "" || pass === ""){
+      error.innerHTML = "Email and Password must be entered"
+        
+    }
 
+
+});
+function loginValidate(user, password){
+
+}
