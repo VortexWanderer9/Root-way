e21e02d5cc1c9c19b65a6c738eeaa40563121929
Tyrell Wellick <keshavniure99@gmail.com>
2025-09-08 08:24:38 -0800

    trying to implement backend

---
+++ data/user.js
@@ -0,0 +1,4 @@
+export const userData = [{
+    id: "Admin",
+    password: 123456
+},{},{}]
--- script/login.js
+++ script/login.js
@@ -1,12 +1,10 @@
 
+import {userData } from '../data/user.js'
 
 const button = document.querySelector(".button");
 const email = document.querySelector('#email-input');
 const password = document.querySelector('#password-input');
 const error = document.querySelector('.error-message');
-
-
-
 
 button.addEventListener('click', (e) =>{
    e.preventDefault()
@@ -15,8 +13,16 @@ button.addEventListener('click', (e) =>{
     let pass = password.value.trim();
     if(user === "" || pass === ""){
         error.textContent = "Email and Password must be entered"
-    }
+    } else  loginValidate(user, pass)
+   
 });
 function loginValidate(user, password){
+
+   userData.forEach((data) =>{
+    let isID = data.id == user
+    let isPass = data.password
+    console.log(isID, isPass);
+   })
+// window.location.href = 'src/create.html'
 
 }
