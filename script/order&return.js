e9c5f78943b949c5d3e63ceacf2304585b14393d
Tyrell Wellick <keshavniure99@gmail.com>
2025-10-04 03:25:31 -0800

    feat(order): implement product order list

--- script/order&return.js
+++ script/order&return.js
@@ -1,2 +1,36 @@
 import {order} from "../data/orderItem.js"
+import { products } from '../data/product.js'
+const container = document.querySelector('.grid-container');
+
+
+let html =" ";
+order.forEach((item) =>{
+    let matchingProduct = products.find((p) => p.id === item.productId);
+     html += `
+     <div class="card">
+                <div class="return-image">
+                    <img src="${matchingProduct.image}" alt="image-of-product">
+                </div>
+                <div class="body-info">
+                     <p class="product-name">${matchingProduct.name}</p>
+                    <p class="quantity">Quantity: <span>${item.quantity}</span></p>
+                   </div>
+                 <div class="buttons">
+                   <div class="track-location border-1">
+                    <img width="20" src="images/icons/location.svg" alt="">
+                    <span>Track Item</span>
+                   </div>
+                   <div class="purchase-item border-1">
+                    <img width="20" src="images/icons/loop.svg" alt="">
+                    <span>Order Again</span>
+                   </div>
+                   <div class="return border-1">
+                    <img width="20" src="images/icons/return-box.svg" alt="">
+                    <span>Return Item</span>
+                   </div>
+                   </div>
+            </div>
+    `
+})
+container.innerHTML = html;
 
