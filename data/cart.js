f85b094b9ea5bda0b8aec6cc18062e9d6105b80d
Tyrell Wellick <keshavniure99@gmail.com>
2025-10-07 05:09:00 -0800

    fix bug

--- data/cart.js
+++ data/cart.js
@@ -1,64 +1,64 @@
-export let cart = [];
+    export let cart = [];
 
-getCartFromLocalStorage()
-function getCartFromLocalStorage(){
-    cart = JSON.parse(localStorage.getItem('cart')) 
-   if(!cart){
-    cart =  [
-              {
-                productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-                quantity: 2,
-              },
-              {
-                productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-                quantity: 1,
-              },
-    ];
-}
-}
-
- function addToLocalStorage(){
-    localStorage.setItem('cart', JSON.stringify(cart));
-}
-export function addToCart(productId, quantity){
-    let matchingProduct = cart.find(item => item.productId === productId);
-
-    if (matchingProduct) {
-        matchingProduct.quantity += quantity;
-    } else {
-        cart.push({
-            productId: productId,
-            quantity: quantity,
-        });
+    getCartFromLocalStorage()
+    function getCartFromLocalStorage(){
+        cart = JSON.parse(localStorage.getItem('cart')) 
+    if(!cart){
+        cart =  [
+                {
+                    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                    quantity: 2,
+                },
+                {
+                    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+                    quantity: 1,
+                },
+        ];
     }
-    addToLocalStorage();
-}
-
-    export function removeFromCart(productId){
-        let newCart = [];
-        cart.forEach((item) =>{
-            if(item.productId !== productId){
-                newCart.push(item)
-            }
-        })
-    cart = newCart;
-    addToLocalStorage()
     }
 
-export function updateQuantity(productId, quantity){
-    let matchingProduct = cart.find(item => item.productId === productId);
-  const newQuantity = Number(quantity)
-    if(newQuantity <= 0) return;
-
-    if(matchingProduct){
-        matchingProduct.quantity = newQuantity;
+    function addToLocalStorage(){
+        localStorage.setItem('cart', JSON.stringify(cart));
     }
-    addToLocalStorage();
-};
+    export function addToCart(productId, quantity){
+        let matchingProduct = cart.find(item => item.productId === productId);
 
-export function clearCartItem(){
-   let newCart = [];
-    cart = newCart;
-    addToLocalStorage();
-}
+        if (matchingProduct) {
+            matchingProduct.quantity += quantity;
+        } else {
+            cart.push({
+                productId: productId,
+                quantity: quantity,
+            });
+        }
+        addToLocalStorage();
+    }
+
+        export function removeFromCart(productId){
+            let newCart = [];
+            cart.forEach((item) =>{
+                if(item.productId !== productId){
+                    newCart.push(item)
+                }
+            })
+        cart = newCart;
+        addToLocalStorage()
+        }
+
+    export function updateQuantity(productId, quantity){
+        let matchingProduct = cart.find(item => item.productId === productId);
+    const newQuantity = Number(quantity)
+        if(newQuantity <= 0) return;
+
+        if(matchingProduct){
+            matchingProduct.quantity = newQuantity;
+        }
+        addToLocalStorage();
+    };
+
+    export function clearCartItem(){
+    let newCart = [];
+        cart = newCart;
+        addToLocalStorage();
+    }
 
--- script/nav.js
+++ script/nav.js
@@ -1,22 +1,22 @@
 
-const nav = document.getElementById('navbar');
-window.addEventListener('scroll', () =>{
-    if(window.scrollY > 80){
-        nav.classList.add('scrolled')
-    }
-    else{
-        nav.classList.remove('scrolled')
-    }
-})
-const menuBtn = document.querySelector('.menu');
-const sideNav = document.querySelector('.side-nav');
-const closeBtn =  document.querySelector('.close');
-menuBtn.addEventListener('click', (e) =>{
-    e.preventDefault()
-    sideNav.classList.toggle('nav-active');
-   closeBtn.addEventListener('click', (e) =>{
-       sideNav.classList.remove('nav-active');
+    const nav = document.getElementById('navbar');
+    window.addEventListener('scroll', () =>{
+        if(window.scrollY > 80){
+            nav.classList.add('scrolled')
+        }
+        else{
+            nav.classList.remove('scrolled')
+        }
+    })
+    const menuBtn = document.querySelector('.menu');
+    const sideNav = document.querySelector('.side-nav');
+    const closeBtn =  document.querySelector('.close');
+    menuBtn.addEventListener('click', (e) =>{
+        e.preventDefault()
+        sideNav.classList.toggle('nav-active');
+    closeBtn.addEventListener('click', (e) =>{
+        sideNav.classList.remove('nav-active');
+        });
     });
-});
 
 
